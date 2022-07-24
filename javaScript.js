4ebc2a877fe7b0149d5e16dfb1849b5c8f6501ae
Malinimozhiyan <malinimanimozhiyan@gmail.com>
2022-07-24 17:48:31 +0530

    map,filter,reduce and for each operations without using inbuilt function

---
+++ javaScript.js
@@ -0,0 +1,37 @@
+numbers = [18,21,13,40,5]
+sum = 0;
+function doubleMap(number) {
+    return number*number;
+}
+function passOrFail(number) {
+    if (mark >= 35){
+      return true;
+   }
+   return false;
+}
+function sumArray(number) {
+    sum = sum+number;
+    return sum;
+}
+function getElement(number) {
+    return number;
+}
+const calculate = function(numbers,logic) {
+    let output = []
+    for(iter=0;iter < numbers.length;iter++) {
+        result = logic(numbers[iter]);
+        if(result)
+        output.push(result);
+    }
+    return output;
+    
+}
+console.log("map function without inbuilt function");
+console.log(calculate(numbers,doubleMap));
+console.log("filter function without inbuilt function");
+console.log(calculate(numbers,passOrFail));
+console.log("reduce function without inbuilt function");
+calculate(numbers,sumArray);
+console.log(sum);
+console.log("for each function without inbuilt function");
+console.log(calculate(numbers,getElement));
